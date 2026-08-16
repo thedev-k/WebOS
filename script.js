6fd889135f1da83824ee2e35614507f35d2e064f
Kabeer Mandhan <kabeermandhan07@gmail.com>
2026-08-17 00:50:35 +0500

    Implement draggable window feature and update layout in index.html. Replaced dragable window with bat logo

---
+++ script.js
@@ -0,0 +1,31 @@
+dragElement(document.getElementById("window"));
+function dragElement(elmnt) {
+    var x1 = 0, y1 = 0, x2 = 0, y2 = 0;
+    if (document.getElementById(elmnt.id + "header")) {
+        document.getElementById(elmnt.id + "header").onmousedown = startDragging;
+    } else {
+        elmnt.onmousedown = startDragging;
+    }
+    function startDragging(e) {
+        e = e || window.event;
+        e.preventDefault();
+        x2 = e.clientX;
+        y2 = e.clientY;
+        document.onmouseup = stopDragging;
+        document.onmousemove = dragElement; 
+    }
+    function dragElement(e) {
+        e = e || window.event;
+        e.preventDefault();
+        x1 = x2 - e.clientX;
+        y1 = y2 - e.clientY;
+        x2 = e.clientX;
+        y2 = e.clientY;
+        elmnt.style.top = (elmnt.offsetTop - y1) + "px";
+        elmnt.style.left = (elmnt.offsetLeft - x1) + "px";
+    }
+    function stopDragging() {
+        document.onmouseup = null;
+        document.onmousemove = null;
+    }
+}
